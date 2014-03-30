140402e561460f4a7b9395200eed657bbce8725d
Sam <unclesaamm@gmail.com>
2014-03-30 06:59:39 -0400

    fixed repeat bug

--- public/javascripts/newtext.js
+++ public/javascripts/newtext.js
@@ -1,23 +1,25 @@
 var socket = io.connect(window.location.hostname);
+var lastdate = '';
 
 socket.on("connect", function() {
-    console.log("socket.io connected");
     socket.on("text", function (data) {
-        console.log(data["msg"]);
-        var block = $("<div>", {"class": "message"})
-            .append($("<div>", {
-                "class": "text",
-                text: data["msg"]}))
-            .append($("<div>", {
-                "class": "date",
-            text: data["date"]}));
-
-        block.prependTo("#messages")
-        .hide()
-        .css("opacity", 0)
-        .slideDown(500,
-            function() {
-                $(this).animate({opacity:1}, 1000);
-            });
+        if (lastdate != data["date"]) {
+            lastdate = data["date"];
+            var block = $("<div>", {"class": "message"})
+                .append($("<div>", {
+                    "class": "text",
+                    text: data["msg"]}))
+                .append($("<div>", {
+                    "class": "date",
+                text: data["date"]}));
+            
+            block.prependTo("#messages")
+            .hide()
+            .css("opacity", 0)
+            .slideDown(500,
+                function() {
+                    $(this).animate({opacity:1}, 1000);
+                });
+        }
     });
 });
